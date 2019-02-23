7de5ae35c33dd576e78460ffeb83612d6b2527dd
Michael S Corigliano <michael.s.corigliano@gmail.com>
2019-02-23 18:01:17 -0500

    Revert "external/google: Switch to AquaUtils as we dont use ActionUtils"
    This reverts commit fe83ceb3332127efe2bbe66c38a870da186dbf14.
    
    Change-Id: I670e35ffff05ee185a5ecedf5de848dcec36c744

--- src/com/android/systemui/elmyra/actions/CustomActions.java
+++ src/com/android/systemui/elmyra/actions/CustomActions.java
@@ -12,6 +12,7 @@ import com.android.internal.util.du.Utils;
 import com.android.internal.util.du.ActionUtils;
 import com.android.internal.utils.ActionHandler;
 import com.android.internal.utils.Config.ActionConfig;
+import com.android.internal.util.du.ActionUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.assist.AssistManager;
 
@@ -38,5 +39,55 @@ public class CustomActions extends Action {
         String action = ActionConfig.getActionFromDelimitedString(getContext(), actionConfig,
                 ActionHandler.SYSTEMUI_TASK_NO_ACTION);
         ActionHandler.performTask(getContext(), action);
+        mActionSelection = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.SQUEEZE_SELECTION, 0, UserHandle.USER_CURRENT);
+
+        switch (mActionSelection) {
+            case 0: // No action
+            default:
+                break;
+            case 1: // Assistant
+                mAssistManager.startAssist(new Bundle() /* args */);
+                break;
+            case 2: // Voice search
+                launchVoiceSearch(getContext());
+                break;
+            case 3: // Camera
+                launchCamera(getContext());
+                break;
+            case 4: // Flashlight
+                ActionUtils.toggleCameraFlash();
+                break;
+            case 5: // Clear notifications
+                ActionUtils.clearAllNotifications();
+                break;
+            case 6: // Volume panel
+                ActionUtils.toggleVolumePanel(getContext());
+                break;
+            case 7: // Screen off
+                ActionUtils.switchScreenOff(getContext());
+                break;
+            case 8: // Notification panel
+                ActionUtils.toggleNotifications();
+                break;
+            case 9: // Screenshot
+                ActionUtils.takeScreenshot(true);
+                break;
+            case 10: // QS panel
+                ActionUtils.toggleQsPanel();
+                break;
+        }
+    }
+
+    private static void launchCamera(Context context) {
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA_SECURE);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+    }
+
+    private void launchVoiceSearch(Context context) {
+        Intent intent = new Intent(Intent.ACTION_SEARCH_LONG_PRESS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
